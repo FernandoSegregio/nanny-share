7ef1af0b8c7ba513a880bcb02be00f0208b42739
Fernando Segregio <segregio@gmail.com>
2022-04-21 15:41:34 -0300

    feat: add ArticleShare and paragraph

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { ArticleProfile, MainContainer, ShowApp } from './style';
+import {
+  ArticleProfile, ArticleShare, MainContainer, ShowApp,
+} from './style';
 import sarahImage from '../../assets/images/main_images/sarah-profile-image.svg';
 
 export default function Main() {
@@ -11,6 +13,19 @@ export default function Main() {
           <a href="profile">Sarah’s day care available now in North Sydney</a>
           <h5>Wednesday, Thursday, Friday - 7:30 - 5:30</h5>
         </ArticleProfile>
+        <ArticleShare>
+          <p>
+            You have a fantastic home, a fantastic nanny and wouldn’t
+            cutting your costs in half be, well, fantastic?! If only it
+            was easy to connect with other parents to share your costs?
+            Well now it is, with Hapu.
+            <a href="tribe">Hapu means tribe</a>
+            and it’s our foundational 3 tribal principles that empowers you to
+            create and manage your own tribe. A tribe that together
+            has the power to create new affordable solutions in
+            childcare that work for you and your community.
+          </p>
+        </ArticleShare>
       </ShowApp>
     </MainContainer>
   );
