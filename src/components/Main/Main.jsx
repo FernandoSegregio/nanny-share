6987061b2982b55720901e8457e49981a6472df4
Fernando Segregio <segregio@gmail.com>
2022-04-21 13:12:55 -0300

    refactor: exchange component ArticleShow for ArticleProfile

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { ArticleShow, MainContainer, ShowApp } from './style';
+import { ArticleProfile, MainContainer, ShowApp } from './style';
 
 export default function Main() {
   return (
     <MainContainer>
       <ShowApp>
-        <ArticleShow>
+        <ArticleProfile>
           article
-        </ArticleShow>
+        </ArticleProfile>
       </ShowApp>
     </MainContainer>
   );
