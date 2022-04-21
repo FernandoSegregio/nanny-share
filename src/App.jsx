5e6253c6eb33ae950cf2e57b727c59099f0c8d42
Fernando Segregio <segregio@gmail.com>
2022-04-21 01:03:32 -0300

    feat: create component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Header from './components/Header/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      React
+    <div>
+      <Header />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
