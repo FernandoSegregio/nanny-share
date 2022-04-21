c7dcb9c1915b16567eee5bd075c5b21f1b374eca
Fernando Segregio <segregio@gmail.com>
2022-04-21 19:30:55 -0300

    test: checks for a inputs with data testid

--- src/components/Main/Main.test.jsx
+++ src/components/Main/Main.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Main from './Main';
+import { ArticleProfile } from './style';
 
 describe('Testing component Main', () => {
   it('should be able to show a main element in the document.', () => {
@@ -11,7 +12,7 @@ describe('Testing component Main', () => {
     expect(mainElement).toBeInTheDocument();
   });
   it('should be able to show a article element in the document.', () => {
-    render(<Main />);
+    render(<ArticleProfile />);
 
     const articleElement = screen.getByRole('article');
     expect(articleElement).toBeInTheDocument();
@@ -21,5 +22,16 @@ describe('Testing component Main', () => {
     const altImgProfile = 'profile sarah';
     const imgProfile = screen.getByAltText(altImgProfile);
     expect(imgProfile).toHaveAttribute('src', 'sarah-profile-image.svg');
+  });
+  it('should be able to show an input field with the due data test id.', () => {
+    render(<Main />);
+
+    const inputName = screen.getByTestId('input-name');
+    expect(inputName).toBeInTheDocument();
+    expect(inputName).toHaveValue('');
+
+    const inputEmail = screen.getByTestId('input-email');
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail).toHaveValue('');
   });
 });
