b36af812c0d6e45e62a38942e4e2b1e43ecb3e6e
Fernando Segregio <segregio@gmail.com>
2022-04-21 17:25:34 -0300

    feat: create controlled states

--- src/components/Form/FormPost.jsx
+++ src/components/Form/FormPost.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 
 export default function FormPost() {
+  const [name, setName] = useState('');
+  const [email, setMail] = useState('');
+
   return (
     <div>FormPost</div>
   );
