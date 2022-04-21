a7081d1319b3235e809d89551fd330087dfb6e70
Fernando Segregio <segregio@gmail.com>
2022-04-21 03:02:15 -0300

    feat: add logo icon and ButtonHeader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import HeaderContainer from './style';
+import { HeaderContainer, ButtonHeader } from './style';
+import iconTopHeader from '../../assets/images/icon-top-header.svg';
 
 export default function Header() {
   return (
     <HeaderContainer>
+      <div>
+        <img src={iconTopHeader} alt="logo icon" />
+        <ButtonHeader type="button">Become a Nanny Share Host</ButtonHeader>
+      </div>
       <h2>Easily create or join a local nanny share with Hapu</h2>
     </HeaderContainer>
   );
