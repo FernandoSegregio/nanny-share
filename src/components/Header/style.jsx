3b866ce1088595443c2d8a7d53736f87e0d4b44a
Fernando Segregio <segregio@gmail.com>
2022-04-21 01:36:05 -0300

    style: add header background

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
+import backgroundMobile from '../../assets/images/background-mobile.svg';
 
 const HeaderContainer = styled.header`
-  
+  background-image: url(${backgroundMobile});
+  background-repeat: no-repeat;
+  height: 540px;
+  width: 360px;
 `;
 
 export default HeaderContainer;
