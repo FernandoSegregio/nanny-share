94251c27a8982bcef5c8f0cd2180b49c5963da84
Fernando Segregio <segregio@gmail.com>
2022-04-21 13:35:20 -0300

    feat: add profile image an ArticleProfile

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ArticleProfile, MainContainer, ShowApp } from './style';
+import sarahImage from '../../assets/images/main_images/sarah-profile-image.svg';
 
 export default function Main() {
   return (
     <MainContainer>
       <ShowApp>
         <ArticleProfile>
-          article
+          <img src={sarahImage} alt="profile sarah" />
         </ArticleProfile>
       </ShowApp>
     </MainContainer>
