ea2e40ebadcc534771e6a5728b3d391610f17610
Fernando Segregio <segregio@gmail.com>
2022-04-21 02:16:14 -0300

    style: add background color mobile

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 import backgroundMobile from '../../assets/images/background-mobile.svg';
 
 const HeaderContainer = styled.header`
-  background-image: url(${backgroundMobile});
+  background-image: url(${backgroundMobile}), linear-gradient(var(--color-overlay));
   background-repeat: no-repeat;
+  background-size: 100%;
   height: 540px;
-  width: 360px;
+  width: 100%;
 `;
 
 export default HeaderContainer;
