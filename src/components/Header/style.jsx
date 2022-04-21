ea8ae4c46658f530a36bc8a45efab5bd545e2ea2
Fernando Segregio <segregio@gmail.com>
2022-04-21 02:35:31 -0300

    style: add style for title

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -7,6 +7,16 @@ const HeaderContainer = styled.header`
   background-size: 100%;
   height: 540px;
   width: 100%;
+
+  h2 {
+    color: var(--neutral-color-white-100);
+    font-size: var(--font-size-xl);
+    font-style: var(--font-style-normal);
+    font-weight: var(--font-weight-500);
+    line-height: var(--line-height-m);
+    text-align: center;
+    width: 272px;
+  }
 `;
 
 export default HeaderContainer;
