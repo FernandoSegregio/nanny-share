6e72bd897ae2f336f90615953b7ea4bd911b2efe
Fernando Segregio <segregio@gmail.com>
2022-04-21 12:49:13 -0300

    test: check checks if there is a main component

---
+++ src/components/Main/Main.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Main from './Main';
+
+describe('Testing component Main', () => {
+  it('should be able to show a main element in the document.', () => {
+    render(<Main />);
+
+    const mainElement = screen.getByRole('main');
+    expect(mainElement).toBeInTheDocument();
+  });
+});
