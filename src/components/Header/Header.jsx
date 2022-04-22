de72bfb846f4c9d7ebcb70a0f44d0ce043516baa
Fernando Segregio <segregio@gmail.com>
2022-04-22 16:31:12 -0300

    style: add version desktop for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,25 +6,34 @@ import {
 } from './style';
 import iconTopHeader from '../../assets/images/header_images/icon-top-header.svg';
 import iconPlay from '../../assets/images/header_images/icon-play.svg';
+import imgJaneKate from '../../assets/images/header_images/jane-kate.svg';
 
 export default function Header() {
   return (
     <HeaderContainer>
       <nav>
         <img src={iconTopHeader} alt="logo icon" />
+        <ul>
+          <li>Create Your Nanny Share</li>
+          <li>Browse Shares</li>
+          <li>Our Story</li>
+        </ul>
         <ButtonHeader type="button">Become a Nanny Share Host</ButtonHeader>
       </nav>
       <article>
-        <h2>Easily create or join a local nanny share with Hapu</h2>
-        <p>
-          Hapu is Airbnb for nanny share. Share your home,
-          nanny and costs and create new flexible, affordable
-          solutions in childcare.
-        </p>
-        <Player>
-          <img src={iconPlay} alt="logo play" />
-          <a href="#test">See hapu in action (27 seconds)</a>
-        </Player>
+        <section>
+          <h2>Easily create or join a local nanny share with Hapu</h2>
+          <p>
+            Hapu is Airbnb for nanny share. Share your home,
+            nanny and costs and create new flexible, affordable
+            solutions in childcare.
+          </p>
+          <Player>
+            <img src={iconPlay} alt="logo play" />
+            <a href="#test">See hapu in action (27 seconds)</a>
+          </Player>
+        </section>
+        <img src={imgJaneKate} className="imgHidden" alt="manage nanny share" />
       </article>
     </HeaderContainer>
   );
--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import backgroundMobile from '../../assets/images/header_images/background-mobile.svg';
+import backgroundDesktop from '../../assets/images/header_images/background-desktop.svg';
 
 export const HeaderContainer = styled.header`
   align-items: center;
@@ -12,40 +13,126 @@ export const HeaderContainer = styled.header`
   height: 540px;
   width: 100%;
 
+    @media (min-width: 900px) {
+      background-image: url(${backgroundDesktop}), linear-gradient(var(--color-overlay));
+      background-size: cover;
+      height: 616px;
+    }
+
   nav {
     align-items: flex-end;
     display: flex;
     justify-content: space-between;
     height: 64px;
     width: 88%;
+
+      ul {        
+        height: 0;
+        visibility: hidden;
+        width: 0;
+
+          @media (min-width: 900px) {
+            color: var(--neutral-color-white-100);
+            display: flex;
+            font-size: var(--font-size-xs);
+            font-style: var(--font-style-normal);
+            font-weight: var(--font-weight-500);
+            justify-content: space-around;
+            left: 12%;
+            line-height: 20x;
+            position: absolute;
+            text-align: center;
+            top: 3%;
+            visibility: visible;
+            width: 398px;
+            
+              li {
+                list-style: none;
+                cursor: pointer;
+              }
+          }
+      }
   }
 
-  h2 {
-    color: var(--neutral-color-white-100);
-    font-size: var(--font-size-xl);
-    font-style: var(--font-style-normal);
-    font-weight: var(--font-weight-500);
-    line-height: var(--line-height-m);
-    text-align: center;
-    width: 272px;
-  }
+
+  
+
+
 
   article {
     align-items: center;
     display: flex;
-    height: 298px;
+    height: 450px;
     flex-direction: column;
     row-gap: var(--spacing-xxxs);
-    padding-top: 90px;
+    width: 100%;
+    padding-top: 110px;
+
+      @media (min-width: 900px) {
+        flex-direction: row;
+        justify-content: space-evenly;
+        height: 400px;
+      }
+
+      section {
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      height: 400px;
+      row-gap: var(--spacing-xxxs);
+
+      @media (min-width: 900px) {
+        align-items: flex-start;
+        text-align: left;
+        width: 490px;
+      }
+
+        h2 {
+        color: var(--neutral-color-white-100);
+        font-size: var(--font-size-xl);
+        font-style: var(--font-style-normal);
+        font-weight: var(--font-weight-500);
+        line-height: var(--line-height-m);
+        text-align: center;
+        width: 272px;
+
+          @media (min-width: 900px) {
+            text-align: left;
+            width: 490px;
+          }
+      
+        }   
+
 
       p {
-        color: var(--neutral-color-white-100);
-        font-size: var(--font-size-s);
-        font-style: var(--font-style-normal);
-        font-weight: var(--font-weight-normal);
-        line-height: 28px;
-        text-align: center;
-        width: 328px;
+          color: var(--neutral-color-white-100);
+          font-size: var(--font-size-s);
+          font-style: var(--font-style-normal);
+          font-weight: var(--font-weight-normal);
+          line-height: 28px;
+          text-align: center;
+          width: 328px;
+
+            @media (min-width: 900px) {
+              text-align: left;
+              margin-right: 40px;
+              width: 584px;
+            }
+      }
+    }
+
+      .imgHidden {
+      visibility: hidden;
+      height: 0;
+      width: 0;
+
+        @media (min-width: 900px) {
+          visibility: visible;
+          height: 290px;
+          margin-left: 50px;
+          width: 316px;
+        }
       }
   }
 `;
@@ -67,6 +154,10 @@ export const Player = styled.div`
   justify-content: space-between;
   padding-top: 32px;
   width: 310px;
+
+    @media (min-width: 900px) {
+      text-align: left;
+    }
   
     a {
       color: var(--neutral-color-white-100);
