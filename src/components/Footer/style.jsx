f0637fb890418954c3837c3c0322887317d6da74
Fernando Segregio <segregio@gmail.com>
2022-04-22 08:00:22 -0300

    style: add display flex

--- src/components/Footer/style.jsx
+++ src/components/Footer/style.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const FooterContainer = styled.footer`
   height: 574px;
   width: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default FooterContainer;
